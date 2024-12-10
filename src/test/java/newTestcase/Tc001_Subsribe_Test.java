902893206a570f771809630f22531dd3b042988f
avirajred26 <78291181+avirajred26@users.noreply.github.com>
2024-12-10 15:35:02 +0530

    Update Tc001_Subsribe_Test.java

--- src/test/java/newTestcase/Tc001_Subsribe_Test.java
+++ src/test/java/newTestcase/Tc001_Subsribe_Test.java
@@ -19,26 +19,7 @@ public class Tc001_Subsribe_Test extends TestBase {
 	@Test(priority = 2,dataProvider = "test")
 	public void portfolioTest(String Scenario) throws InterruptedException, IOException, CsvException {
 		
-		objHome.selectMainTabs("All Topics");
-		objtop = new AllTopicPage(driver);
 		
-		TestUtil.selectItemByVisibleText(objtop.sortBy, "Date of topic update");
-		
-		String topicName = csvLibrary.readColValue(Scenario, "topicName");
-		
-		objtop.selectTopic(topicName);
-		
-		String subtopicName = csvLibrary.readColValue(Scenario, "subTopicName");
-		
-		objtop.search_for_subTopic_click_to_suscribe(subtopicName);
-		
-		if(!csvLibrary.readColValue(Scenario, "Unsuscribe").contains("No")) {
-			
-			objHome.selectUserMono("My Subscriptions");
-			
-			objtop.want_to_unsuscribe(subtopicName);
-			
-		}
 	
 		
 	}
