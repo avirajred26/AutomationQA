f4525e26f9cfef7d1742944c70783666df6157ed
avirajred26 <78291181+avirajred26@users.noreply.github.com>
2024-12-10 15:34:08 +0530

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -16,57 +16,6 @@ public class HomePage extends PageBase {
 	public List<WebElement> subTopicNames;
 	
 	
-	@FindBy(xpath = "(//*[name()='svg'][@class='svg-inline--fa fa-angle-down '])[1]")
-	public WebElement userDropDown;
-	
-	
-	@FindBy(xpath = "//div[@class='chakra-menu__group']/child::a")
-	public List<WebElement> menuList;
-	
-	
-
-	public HomePage(WebDriver driver) {
-		setWebDriver(pbDriver);
-	}
-
-	public void selectMainTabs(String tabName) throws InterruptedException {
-		
-		Thread.sleep(2000);
-		
-		waitForElementsToAppearBoolean(subTopicNames);
-		
-		for (WebElement webElement : subTopicNames) {
-			
-			System.out.println(webElement.getText());
-			if(webElement.getText().equalsIgnoreCase(tabName)) {
-				
-				jsExecutorClickOn(webElement);
-				break;
-			}
-		}
-		
-		
-		
-		
-	}
-	
-	public void selectUserMono(String OptionValue) throws InterruptedException {
-		Thread.sleep(3000);
-		
-		clickonsvgElement(userDropDown);
-		Thread.sleep(1000);
-		for (WebElement webElement : menuList) {
-		
-			if(webElement.getText().contains(OptionValue)) {
-				jsExecutorClickOn(webElement);
-				break;
-			}
-			
-		}
-		
-		
-		
-	}
 	
 	
 }
